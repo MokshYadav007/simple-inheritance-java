1c97e19fa19245b7ea21421e534a1ce3f1d5d3b7
Pradeep Singh <125721624+pradeep-003@users.noreply.github.com>
2023-09-14 09:15:38 +0530

    Update AreaInheritance.java

--- AreaInheritance.java
+++ AreaInheritance.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 
 abstract class shape
 {
-    float area;
     float radius;
+    float length;
+    float breadth;
+    float area;
+    Scanner sc= new Scanner(System.in);
     abstract public void input();
     abstract public void calculation();
     public void disp()
@@ -15,61 +18,46 @@ abstract class shape
 
 class Rectangle extends shape
 {
-    float length;
-    float breadth;
-
     public void input()
     {
         System.out.println("The calculation of area for different shapes is below: ");
-        Scanner sc= new Scanner(System.in);
         System.out.println("The length of rectangle is : ");
         length = sc.nextFloat();
         System.out.println("The breadth of rectangle is : ");
-        breadth = sc.nextFloat();      
+        breadth = sc.nextFloat();
     }
-     public void calculation()
-     {
+    public void calculation()
+    {
         area = length*breadth;
-
-     }
-
-
+    }
 }
 
 class Square extends shape
 {
-    float length;
-
     public void input()
     {
-        Scanner sc= new Scanner(System.in);
         System.out.println("The length of square is : ");
         length = sc.nextFloat();
-      sc.close();
     }
-     public void calculation()
-     {
+    public void calculation()
+    {
         area = length*length;
+    }
 
-     }
 
-     
 }
 
 class Circle extends shape
 {
-    
     public void input()
     {
-        Scanner scan= new Scanner(System.in);
         System.out.println("The radius of circle is : ");
-        radius = scan.nextFloat();
-        scan.close();
+        radius = sc.nextFloat();
     }
-     public void calculation()
-     {
+    public void calculation()
+    {
         area = 3.14f * radius * radius;
-     }
+    }
 }
 
 class new2
@@ -83,7 +71,7 @@ class new2
 
 }
 public class AreaInheritance {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Rectangle rec = new Rectangle();
         Square sq = new Square();
@@ -93,5 +81,4 @@ public class AreaInheritance {
         n.poly(sq);
         n.poly(ci);
     }
-
 }
